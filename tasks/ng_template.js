bc9b3303b2a81f0f02f99f099fec16b88a51ab7e
bonho <wormuz@gmail.com>
2017-06-17 15:23:08 +0300

    Fixed processing of the slash in the windows-like pathes of the files

--- tasks/ng_template.js
+++ tasks/ng_template.js
@@ -11,20 +11,21 @@ module.exports = function (grunt) {
 
     grunt.registerMultiTask('ng_template', 'Make external angular templates inline', function () {
 
-        var configs = {
-            TEMPLATE: '<script type="text/ng-template" id="<% filename %>">' + grunt.util.linefeed + '<% content %>' + grunt.util.linefeed + '</script>',
-            partials: [],
-            comment_line: '<!-- ng:templates -->' + grunt.util.linefeed + '<% content %>' + grunt.util.linefeed + '<!-- end ng:templates -->',
-            PATH: require('path'),
-            appDir_path: require('path').resolve(this.options().appDir),
-            endTag: this.options().endTag
-        },
-            BASE_FILE = this.options().appDir + '/' + this.options().indexFile,
+        const configs = {
+                TEMPLATE: `<script type="text/ng-template" id="<% filename %>">${grunt.util.linefeed}<% content %>${grunt.util.linefeed}</script>`,
+                partials: [],
+                comment_line: '<!-- ng:templates -->' + grunt.util.linefeed + '<% content %>' + grunt.util.linefeed + '<!-- end ng:templates -->',
+                PATH: require('path'),
+                appDir_path: require('path').resolve(this.options().appDir),
+                endTag: this.options().endTag,
+                viewsDir: this.options().viewsDir
+            },
+            BASE_FILE = `${this.options().appDir}/${this.options().indexFile}`,
             ABS_PATH = process.cwd();
 
         function stripTemplates() {
 
-            var base_file = grunt.file.read(BASE_FILE),
+            let base_file = grunt.file.read(BASE_FILE),
 
                 stripped_html = base_file.replace(/<script type="text\/ng-template" ([^'"]|"[^"]*"|'[^']*')*?<\/script>/gi, '');
             stripped_html = stripped_html.replace(/\s\s<!-- ng\:templates -->\s+<!-- end ng:templates -->\s\s/g, '');
@@ -45,14 +46,13 @@ module.exports = function (grunt) {
                 return false;
             }
 
-            var src = f.src.filter(function (filepath) {
+            f.src.filter(function (filepath) {
 
                 if (!grunt.file.exists(filepath)) {
-                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    grunt.log.warn(`Source file "${filepath}" not found.`);
                     return false;
-                } else {
-                    return true;
                 }
+                else { return true; }
 
 
             }).map(function (filepath) {
@@ -65,11 +65,13 @@ module.exports = function (grunt) {
 
                             grunt.log.ok(configs.PATH.resolve(abspath)
                                 .replace(configs.appDir_path, '')
-                                .replace(!!(configs.viewsDir) ? new RegExp(`^\/${configs.viewsDir}\/`) : /^\//, ''));
+                                .replace(/\\/g, '/')
+                                .replace((configs.viewsDir) ? new RegExp(`^[\/]{0,1}${configs.viewsDir}\/`) : /^\//, ''));
 
-                            var _filename = configs.PATH.resolve(abspath)
+                            const _filename = configs.PATH.resolve(abspath)
                                     .replace(configs.appDir_path, '')
-                                    .replace(!!(configs.viewsDir) ? new RegExp(`^\/${configs.viewsDir}\/`) : /^\//, ''),
+                                    .replace(/\\/g, '/')
+                                    .replace((configs.viewsDir) ? new RegExp(`^[\/]{0,1}${configs.viewsDir}\/`) : /^\//, ''),
                                 content = grunt.file.read(configs.PATH.resolve(abspath)),
                                 parsed = configs.TEMPLATE.replace('<% filename %>', _filename).replace('<% content %>', content);
 
@@ -87,7 +89,7 @@ module.exports = function (grunt) {
 
         });
 
-        var new_content = configs.partials.reverse().join(grunt.util.linefeed),
+        let new_content = configs.partials.reverse().join(grunt.util.linefeed),
             old_file = grunt.file.read(BASE_FILE),
             BODY_END = configs.endTag ? old_file.search(configs.endTag) : old_file.search('</body>'),
             _begin = BODY_END > -1 ? old_file.substring(0, BODY_END) : old_file,
